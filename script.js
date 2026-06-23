5374d540100737db712f5db4218f5ba5e7662377
mumbinatalie0-spec <mumbinatalie0@gmail.com>
2026-06-22 22:38:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+widow.onload = functioon(){
+ alert("Welcome to Mumbi's Thrift Haven");
+};
+
+setTimeout(functin(){
+           document.getElementById("PromoBox").style.display = "block";
+},3000);
+
+functiom closePromo(){
+  document.getElementById("promobox").style.display = "none";
+},3000);
+
+document.getElementById("userForm").addEventListener("submit",function(event){
+  event.preventDefault();
+  alert("Thank you! Your details have been submitted successfully.");
+});
 
